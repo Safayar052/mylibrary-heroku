a020c8e73e7d9dba6607641f0b0f70dc5dc4bac3
nlsafahm <safayar.ahmed@eci.nl>
2017-03-31 12:20:02 +0200

    Small changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var assert = require('assert');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,3 +19,11 @@ app.listen(app.get('port'), function() {
 });
 
 
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb://safayar:mongoadmin@cluster0-shard-00-00-17g43.mongodb.net:27017,cluster0-shard-00-01-17g43.mongodb.net:27017,cluster0-shard-00-02-17g43.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&authSource=admin&ssl=true";
+MongoClient.connect(uri, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+  db.close();
+});
